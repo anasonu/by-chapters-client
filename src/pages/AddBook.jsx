e556faf42cacf55dc939aaf64bcb5fcad7a44412
Ana Sobrino <anasonu@hotmail.com>
2022-06-06 15:25:12 +0200

    Add functionality to AddBook form. Add styles to book detail page

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { addBookService } from '../services/book.services';
 
 function AddBook() {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  // const [img, setImg] = useState(null);
+
+  // const handleImgChange = (event) => setImg(event.target.value);
+  const handleTitleChange = (event) => setTitle(event.target.value);
+  const handleDescriptionChange = (event) => setDescription(event.target.value);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const newBook = {
+        // img,
+        title,
+        description,
+      };
+
+      await addBookService(newBook);
+      navigate("/");
+    } catch (error) {
+      navigate("/error");
+    }
+  };
+
   return (
-    <div>AddBook</div>
+    <div>
+      <h2>Crea tu propio libro</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="img">Imagen de portada</label>
+        <input type="text" name="img" />
+        <br />
+        <br />
+        <label htmlFor="title">Título del libro</label>
+        <input type="text" name="title" onChange={handleTitleChange} value={title} />
+        <br />
+        <br />
+        <label htmlFor="description">¿Sobre qué trata tu libro?</label>
+        <textarea name="description" cols="30" rows="10" onChange={handleDescriptionChange} value={description}></textarea>
+        <br />
+        <br />
+        <button>Crear libro</button>
+      </form>
+    </div>
   )
 }
 
--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -29,13 +29,13 @@ function BookDetail() {
 
   return (
     <div className="book-detail-container">
-      <img src={bookDetail.img} alt={bookDetail.title} />
+      <img src={bookDetail.img} alt={bookDetail.title} className="book-detail-img" />
       <div className="flex-space-between">
-        <h2>{bookDetail.title}</h2>
+        <h2 className="book-detail-title">{bookDetail.title}</h2>
         <button className="add-chapter-btn">+ Capítulo</button>
       </div>
-      <h3>{bookDetail.author.username}</h3>
-      <p>{bookDetail.description}</p>
+      <h3 className="book-detail-author">{bookDetail.author.username}</h3>
+      <p className="book-detail-description">{bookDetail.description}</p>
       <ChaptersList />
     </div>
   );
